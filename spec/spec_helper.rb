1276371ac6835f8f10a3672273bc0a49a54f023e
Rory MacDonald <rory@madebymade.co.uk>
2016-06-14 12:29:16 +0100

    Cleanup spec_helper file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,4 @@
-# Configure Rails Environment
-ENV["RAILS_ENV"] = "test"
-
-require File.expand_path("../dummy/config/environment.rb",  __FILE__)
-
-require 'ffaker'
-require 'rspec/rails'
-
-Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
-
-require 'spree/testing_support/factories'
-require 'spree/testing_support/controller_requests'
-require 'spree/testing_support/authorization_helpers'
+Dir[File.join(File.dirname(__FILE__), "support/*.rb")].each {|f| require f }
 
 RSpec.configure do |config|
   config.mock_with :rspec
---
+++ spec/support/spree.rb
@@ -0,0 +1,4 @@
+require 'ffaker'
+require 'spree/testing_support/factories'
+require 'spree/testing_support/controller_requests'
+require 'spree/testing_support/authorization_helpers'
