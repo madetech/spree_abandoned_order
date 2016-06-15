0384ec5183f862af323ec285bbc6392d99eacd17
Rory MacDonald <rory@madebymade.co.uk>
2016-06-15 15:59:30 +0100

    Refactor timeframe logic

--- lib/spree_abandoned_orders/lookup_query.rb
+++ lib/spree_abandoned_orders/lookup_query.rb
@@ -6,10 +6,16 @@ module Spree
       end
 
       def find_each(&block)
-        begin_timeframe = (Time.zone.now - Spree::AbandonedOrdersConfig.inactivity_for)
-        end_timeframe = (Time.zone.now - Spree::AbandonedOrdersConfig.ignore_after)
-        @relation.where(updated_at: (end_timeframe..begin_timeframe)).find_each(&block)
+        @relation.where(updated_at: within_timeframe).find_each(&block)
       end
+
+      private
+      def within_timeframe
+        begin_at = (Time.zone.now - Spree::AbandonedOrdersConfig.ignore_after)
+        end_at = (Time.zone.now - Spree::AbandonedOrdersConfig.inactivity_for)
+        (begin_at..end_at)
+      end
+
     end
   end
 end
